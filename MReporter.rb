fa61efb019341826582e901c8d37c662016e1471
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-25 00:50:52 +0300

    final working version

--- MReporter.rb
+++ MReporter.rb
@@ -1,14 +1,19 @@
 require 'net/smtp'
 require 'subprocess'
 
+COMMAND = ""
 EMAIL = ""
 PASSWORD = ""
+
+
+output = Subprocess.check_output([COMMAND])
 message = <<BODY
-Test mail
+Command Results
 BODY
 
-message = " ** MRepporter ** \n" + message
+message = " MRepporter  \n" + message + output
 
 Net::SMTP.start('smtp.gmail.com', 587, 'localhost', EMAIL, PASSWORD, :plain) do |smtp|
   smtp.send_message message, EMAIL, EMAIL
 end
+
