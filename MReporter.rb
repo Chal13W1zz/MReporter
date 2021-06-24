8a219fda852d865aaea8e13218ca142b6e65f1b7
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-24 23:59:12 +0300

    working

--- MReporter.rb
+++ MReporter.rb
@@ -1,1 +1,14 @@
 require 'net/smtp'
+
+
+EMAIL = ""
+PASSWORD = ""
+message = <<BODY
+
+BODY
+
+message = " ** MRepporter ** \n" + message
+
+Net::SMTP.start('smtp.gmail.com', 587, 'localhost', EMAIL, PASSWORD, :plain) do |smtp|
+  smtp.send_message message, EMAIL, EMAIL
+end
